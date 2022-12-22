dd642c3587b44e64e2efd9ef4762ab00dbbd0e9c
iamhassan <hassan.elouardy06@gmail.com>
2022-12-22 22:42:28 +0100

    Fix errors

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import './style.css';
-import {form, infos, refresh, url} from './Modules/variables';
+import { form, infos, refresh, url } from './Modules/variables';
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  const userScore = { 
-    user : e.target.user.value,
-    score : e.target.score.value,
-  }
+  const userScore = {
+    user: e.target.user.value,
+    score: e.target.score.value,
+  };
 
   const postRequest = {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(userScore),
-  }
+  };
 
-await fetch(url, postRequest)
-.then((res) => res.json())
-} );
+  await fetch(url, postRequest).then((res) => res.json());
+});
 
-fetch(url).then((res) => res.json()).then((data) => data.result.map(resp => addNewUser(resp)));
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => data.result.map((resp) => addNewUser(resp)));
 
 const addNewUser = (data) => {
-  infos.innerHTML += `<div class ="info">${data.user} : ${data.score} </div>`
-}
+  infos.innerHTML += `<div class ="info">${data.user} : ${data.score} </div>`;
+};
 
 refresh.addEventListener('click', () => {
   window.location.reload();
 });
-
-
