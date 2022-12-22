d07ae82c0ef2bfee573f8bc52cc1b23f9ec4ccec
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2022-12-22 22:46:35 +0100

    Update variables.js

--- src/Modules/variables.js
+++ src/Modules/variables.js
@@ -1,7 +1,6 @@
 const form = document.querySelector('.form');
 const infos = document.querySelector('.infos');
 const refresh = document.querySelector('.refresh');
-const url =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LNcstlj7EXE5sePi5YgH/scores/';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LNcstlj7EXE5sePi5YgH/scores/';
 
 export { form, infos, refresh, url };
