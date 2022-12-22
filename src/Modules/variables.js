e2ee2ce88b38a05675903bf6f358145c5bedd081
iamhassan <hassan.elouardy06@gmail.com>
2022-12-22 22:34:31 +0100

    add api script

---
+++ src/Modules/variables.js
@@ -0,0 +1,7 @@
+
+const form = document.querySelector('.form');
+const infos = document.querySelector('.infos');
+const refresh = document.querySelector('.refresh');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LNcstlj7EXE5sePi5YgH/scores/';
+
+export {form, infos, refresh, url}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import './style.css';
+import {form, infos, refresh, url} from './Modules/variables';
 
-const element = document.querySelector('.infos');
-element.innerHTML = `
-  <div class="score">
-    <p>Name : 100</p>
-    </div>
-    <div class="score">
-    <p>Name : 80</p>
-    </div>
-    <div class="score">
-    <p>Name : 90</p>
-  </div>
-`;
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const userScore = { 
+    user : e.target.user.value,
+    score : e.target.score.value,
+  }
+
+  const postRequest = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userScore),
+  }
+
+await fetch(url, postRequest)
+.then((res) => res.json())
+} );
+
+fetch(url).then((res) => res.json()).then((data) => data.result.map(resp => addNewUser(resp)));
+
+const addNewUser = (data) => {
+  infos.innerHTML += `<div class ="info">${data.user} : ${data.score} </div>`
+}
+
+refresh.addEventListener('click', () => {
+  window.location.reload();
+});
+
+
